12a2ddcbcda8fbdcb2e16188e7521b00820eb174
tsunodajapa <gabrieltsunoda@gmail.com>
2023-05-31 16:32:59 -0300

    fix

--- src/modules/game/components/Inputs/InputBox/index.tsx
+++ src/modules/game/components/Inputs/InputBox/index.tsx
@@ -1,8 +1,10 @@
 import { useFormContext } from 'react-hook-form';
 
+import { useEffect, useRef } from 'react';
 import useFocus from '@/modules/game/hooks/useFocus';
 import useKeyEvents from '@/modules/game/hooks/useKeyEvents';
 import useInputVariant from '@/modules/game/hooks/useInputVariant';
+import { useInputStore } from '@/modules/game/stores/InputStore';
 import styles from './styles.module.scss';
 import { type InputRowIndex } from '../InputGroup';
 import type { FormFields } from '@/modules/game/validators/input';
@@ -19,14 +21,24 @@ const InputBox = ({
   index, isActiveRow, indexRow,
 }: InputBoxProps) => {
   const inputName = `value.${index}` as const;
+  const ref = useRef();
+  const setCurrentInputElement = useInputStore((state) => state.setCurrentInputElement);
 
   const { register } = useFormContext<FormFields>();
+  useEffect(() => {
+    if (index === 0) {
+      console.log(ref.current, register(inputName));
+      setCurrentInputElement(ref.current);
+    }
+  }, []);
+
   const { handleKeyDown, handleKeyUp } = useKeyEvents(inputName);
   const handleFocus = useFocus(index, isActiveRow);
   const variant = useInputVariant({ index, indexRow, isActiveRow });
 
   return (
-    <input
+    <div
+      ref={ref}
       className={styles.container}
       type='text'
       inputMode='none'
@@ -36,7 +48,7 @@ const InputBox = ({
       onFocus={handleFocus}
       onKeyUp={handleKeyUp}
       onKeyDown={handleKeyDown}
-      {...register(inputName)}
+      // {...register(inputName)}
     />
   );
 };
--- src/modules/game/components/Keyboard/Key/index.tsx
+++ src/modules/game/components/Keyboard/Key/index.tsx
@@ -2,6 +2,7 @@
 
 import { useInputStore } from '@/modules/game/stores/InputStore';
 import { useKeysStore } from '@/modules/game/stores/KeysStore';
+import { getAllowedElement } from '@/shared/helpers/hasElement';
 import styles from './styles.module.scss';
 
 interface KeyProps {
@@ -9,26 +10,55 @@ interface KeyProps {
 }
 
 const Key = ({ value }: KeyProps) => {
-  const { currentInputElement, currentFormIndex } = useInputStore((state) => state);
+  const { currentInputElement, currentFormIndex, setCurrentInputElement } = useInputStore((state) => state);
   const usedKey = useKeysStore((state) => state.usedKeys[value]);
   const formReference = value === 'ENTER' ? `form-${currentFormIndex}` : undefined;
   const buttonType = value === 'ENTER' ? 'submit' : 'button';
 
-  const handleClick = () => {
-    currentInputElement?.focus();
+  const handleClick = (event) => {
+    console.log(currentInputElement);
+    if (!currentInputElement) {
+      return;
+    }
+    // currentInputElement.focus();
+    const { previousElementSibling, nextElementSibling } = currentInputElement;
 
-    const keyToPress = value === '<' ? 'Backspace' : value;
+    const isLetterKey = /^[a-zA-Z]$/.test(value);
+    const isArrowLeftKey = value === 'ArrowLeft';
+    const isArrowRightKey = value === 'ArrowRight';
+    const isBackSpaceKey = value === '<';
+    // const previousInput = getAllowedElement(previousElementSibling, 'INPUT');
+    // const nextInput = getAllowedElement(nextElementSibling, 'DIV');
 
-    currentInputElement?.dispatchEvent(new KeyboardEvent('keyup', { key: keyToPress, bubbles: true }));
-    // return false;
+    if (isLetterKey) {
+      currentInputElement.innerHTML = value;
+    }
+
+    if (isBackSpaceKey) {
+      previousElementSibling.innerHTML = '';
+    }
+    // console.log(nextInput, previousInput);
+    if (isLetterKey || isArrowRightKey) {
+      setCurrentInputElement(nextElementSibling);
+      // nextInput?.focus();
+    }
+    if (isArrowLeftKey || isBackSpaceKey) {
+      setCurrentInputElement(previousElementSibling);
+    }
+
+    // const keyToPress = value === '<' ? 'Backspace' : value;
+
+    // currentInputElement?.dispatchEvent(new KeyboardEvent('keyup', { key: keyToPress, bubbles: true }));
+    // // return false;
   };
+
   return (
     <button
       className={styles.container}
       form={formReference}
       type={buttonType}
-      onClick={handleClick}
       data-variant={usedKey}
+      onClick={handleClick}
     >
       {value}
     </button>
