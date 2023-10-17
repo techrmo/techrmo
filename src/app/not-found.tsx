57a242e10a03be7336cb3a1702c8291f0117afa1
tsunodajapa <gabrieltsunoda@gmail.com>
2023-10-17 20:15:00 -0300

    feat: page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,36 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import InputBoxUI from '@/shared/components/ui/InputBox/InputBoxUI';
+import Button from '@/shared/components/ui/Button';
+import styles from '@/shared/scss/not-found.module.scss';
 
 export default function NotFound() {
   return (
-    <main>
+    <main className={styles.container}>
+      <Image
+        src="/logo-medium.svg"
+        alt="Logo techrmo"
+        width={150}
+        height={50}
+        crossOrigin="anonymous"
+      />
       <h1>
         A <strong>palavra</strong> do dia não está aqui não &#128064;
       </h1>
-      <div>
-        <InputBoxUI defaultValue="4" variant="correct" />
-        <InputBoxUI defaultValue="0" variant="incorrect" />
-        <InputBoxUI defaultValue="4" variant="bad-position" />
-      </div>
-      <h2>Página não encontrada</h2>
-      <Link href="/game">Voltar para o jogo</Link>
+      <section>
+        <div>
+          <InputBoxUI defaultValue="4" variant="correct" />
+          <InputBoxUI defaultValue="0" variant="incorrect" />
+          <InputBoxUI defaultValue="4" variant="bad-position" />
+        </div>
+        <h2>Página não encontrada</h2>
+      </section>
+      <footer>
+        <Button asChild variant="outlined-green">
+          <Link href="/game">Voltar para o jogo</Link>
+        </Button>
+      </footer>
     </main>
   );
 }
