7509ad333b98ad9b29bc4707aec3bb91dbcc0564
tsunodajapa <gabrieltsunoda@gmail.com>
2023-10-20 10:51:23 -0300

    fix: refresh route

--- src/app/api/(services)/players/getPlayerByUid.ts
+++ src/app/api/(services)/players/getPlayerByUid.ts
@@ -3,15 +3,9 @@ import { userSchema } from '@/shared/schema/userSchema';
 import { getCollection } from '../firebaseAdmin/firestore';
 
 export const getPlayerByUid = async (uid: string) => {
-  try {
-    const response = await getCollection('players').doc(uid).get();
+  const response = await getCollection('players').doc(uid).get();
 
-    const player = userSchema.parse(response.data());
+  const player = userSchema.parse(response.data());
 
-    return player;
-  } catch (error) {
-    console.error(error);
-
-    return null;
-  }
+  return player;
 };
