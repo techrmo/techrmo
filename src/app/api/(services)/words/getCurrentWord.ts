ed6ef9e66230592cb1f3e32a43b7d40ff9b11429
tsunodajapa <gabrieltsunoda@gmail.com>
2023-10-23 23:33:12 -0300

    fix: remove test

--- src/app/api/(services)/words/getCurrentWord.ts
+++ src/app/api/(services)/words/getCurrentWord.ts
@@ -3,11 +3,6 @@ import { requestGraphQl } from '../hygraph';
 import { getCurrentWordSchema } from './validators/getCurrentWord';
 
 export const getCurrentWord = async () => {
-  return {
-    value: 'TESTE',
-    explanations: [],
-  };
-
   const query = `
     query Words {
       words(where: { isCurrent: true }) {
